76b45068466f63cf2714f68683d335247d7807cc
jessicacfsb <jessica.cfsb@gmail.com>
2021-10-22 18:13:06 -0300

    exercicios de primeiros programas

---
+++ primeiros programas.py
@@ -0,0 +1,53 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ('Alô Mundo')
+Alô Mundo
+
+Print ('Alô Mundo')
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Print ('Alô Mundo')
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+print ('Alô Mundo')
+Alô Mundo
+
+import this
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+dir ('abacate')
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+preço = 9.6666
+print (f'Preço é R${preço:.2f}')
+Preço é R$9.67
+
+==================================== RESTART: Shell ====================================
+aasda
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    aasda
+NameError: name 'aasda' is not defined
+print ('Alô Mundo')
+Alô Mundo
+a = 42
+b = 5
+b * a
+210
